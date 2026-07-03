cf7daa0a61c981956b3971e40f9c20aeb505a883
FontVicius <prog.sevenrockradio@gmail.com>
2026-07-03 22:59:39 +0200

    build: vue assets and ui changes

--- public/build/assets/x-DXLbTbCT.js
+++ public/build/assets/x-DXLbTbCT.js
@@ -1,4 +1,4 @@
-import{c as e}from"./createLucideIcon-Dtm3OUbu.js";/**
+import{c as e}from"./createLucideIcon-DnqSIWb8.js";/**
  * @license lucide-vue-next v1.0.0 - ISC
  *
  * This source code is licensed under the ISC license.
